b1d3a3a6e4a864a615137feadd8eff7b0c1ec5bf
DanielV <work@MacBook-Pro-de-Daniel.local>
2023-01-28 20:45:35 -0500

    first commit

---
+++ MoviesTaller/MoviesTallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTallerApp.swift
+//  MoviesTaller
+//
+//  Created by Daniel Vargas on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesTallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
